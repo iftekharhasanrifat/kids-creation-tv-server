e964aeeca17fa832cde234abb4de4dcdeadded3f
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-07-15 17:42:51 +0600

    image api ready

--- index.js
+++ index.js
@@ -2,29 +2,44 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const authRoute  = require('./routes/auth');
-const adminRoute  = require('./routes/admins');
-const kidsNewsRoute  = require('./routes/kidsNews');
-const programRoute  = require('./routes/programs');
-const upComingProgramRoute  = require('./routes/upcomingPrograms');
+const authRoute = require('./routes/auth');
+const adminRoute = require('./routes/admins');
+const kidsNewsRoute = require('./routes/kidsNews');
+const programRoute = require('./routes/programs');
+const upComingProgramRoute = require('./routes/upcomingPrograms');
+const multer = require('multer');
 
-
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended:true}));
 dotenv.config();
-app.use(express.json())
-
+app.use(express.json());
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
 })
-.then(console.log("Connected to Mongo"))
-.catch(err => console.log(err));
+    .then(console.log("Connected to Mongo"))
+    .catch(err => console.log(err));
 
-app.use("/api/auth",authRoute);
-app.use("/api/admins",adminRoute);
-app.use("/api/kidsNews",kidsNewsRoute);
-app.use("/api/programs",programRoute);
-app.use("/api/upcomingPrograms",upComingProgramRoute);
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, "hello1.jpg");
+    }
+})
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+})
+
+app.use("/api/auth", authRoute);
+app.use("/api/admins", adminRoute);
+app.use("/api/kidsNews", kidsNewsRoute);
+app.use("/api/programs", programRoute);
+app.use("/api/upcomingPrograms", upComingProgramRoute);
 
 app.listen("5000", () => {
     console.log("backend is running")
